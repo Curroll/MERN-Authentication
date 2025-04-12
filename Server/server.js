8a79b2656498e683c754ee780bbde8ce0d6edcc5
Curroll <dhankarprince02@gmail.com>
2025-04-12 10:35:59 +0530

    unexpected

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000;
 
 
 const allowedOrigins = [
-  'http://localhost:5173',
+  'http://localhost:5174',
   'https://mern-authentication-frontend-6mtq.onrender.com'
 ];
 // Middlewares
